16a250f2046abac1f6d9efac2b524194b17a6494
smit9924 <smit.dpatel9924@gmail.com>
2025-03-23 22:10:39 +0530

    list down cart items dynamically

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,9 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { FODD_ITEM_MENU } from '../common/appConstants';
+import { FILE_UPLOAD_URL, FODD_ITEM_MENU, IMAGE_FILE_DIRECTORTY } from '../common/appConstants';
 import { RouterModule } from '@angular/router';
 import { DataService } from '../services/data.service';
-import { ICartItems } from '../common/models/interfaces';
+import { ICartItems, IMediaDataModel } from '../common/models/interfaces';
+import { FOOD_ITEM_QUANTITY_UNIT, FOOD_ITEM_TYPE } from '../common/appEnums';
 
 @Component({
   selector: 'app-cart',
@@ -17,21 +18,46 @@ import { ICartItems } from '../common/models/interfaces';
 })
 export class CartComponent implements OnInit {
   public FODD_ITEM_MENU = FODD_ITEM_MENU;
+  public FOOD_ITEM_TYPE = FOOD_ITEM_TYPE;
   public cartItemsList: ICartItems[] | null = []
 
   constructor(
     private dataService: DataService
-  ) {}
+  ) { }
 
   public async ngOnInit(): Promise<void> {
     this.cartItemsList = await this.dataService.getCartItemListData();
   }
 
   public isCartEmpty(): boolean {
-    if(this.cartItemsList === null || this.cartItemsList.length === 0) {
+    if (this.cartItemsList === null || this.cartItemsList.length === 0) {
       return true;
     }
     return false;
   }
 
+  public getImageURL(imageData: IMediaDataModel | undefined): string {
+    if (imageData) {
+      const imageURL = FILE_UPLOAD_URL + IMAGE_FILE_DIRECTORTY + imageData.guid + '.' + imageData.extension;
+      return imageURL;
+    }
+    return "";
+  }
+
+  public getItemQuantityWithUnit(quantity: number, unitType: FOOD_ITEM_QUANTITY_UNIT): string {
+      let quantityWithUnit = `${quantity} `;
+      switch (unitType) {
+        case FOOD_ITEM_QUANTITY_UNIT.PIECES:
+          quantityWithUnit += 'pcs.';
+          break;
+        case FOOD_ITEM_QUANTITY_UNIT.GRAM:
+          quantityWithUnit += 'gm';
+          break;
+        case FOOD_ITEM_QUANTITY_UNIT.MILLI_LITER:
+          quantityWithUnit += 'ml';
+          break;
+      }
+  
+      return quantityWithUnit;
+    }
 }
