31fad554d12fdc96569c0ae537e6b3e2bef3e916
smit9924 <smit.dpatel9924@gmail.com>
2025-04-11 06:33:21 +0530

    implement contact us form template

--- src/app/contact-us/contact-us.component.ts
+++ src/app/contact-us/contact-us.component.ts
@@ -1,12 +1,48 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ContactUsFormModel } from '../common/models/contact-us-form-model';
+import { DataService } from '../services/data.service';
+import { ToastService } from '../services/toast.service';
+import { PreLoaderService } from '../services/pre-loader.service';
+import { API_CONTACT_US_GET_SUBJECTS } from '../common/apiConstants';
+import { IAPIResponse, IContactUsSubject } from '../common/models/interfaces';
 
 @Component({
   selector: 'app-contact-us',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    FormsModule
+  ],
   templateUrl: './contact-us.component.html',
   styleUrl: './contact-us.component.scss'
 })
-export class ContactUsComponent {
+export class ContactUsComponent implements OnInit {
+  public contactUsFormModel: ContactUsFormModel = new ContactUsFormModel();
+  public subjects: IContactUsSubject[] = [];
 
+  constructor(
+    private dataService: DataService,
+    private toastService: ToastService,
+    private preloaderService: PreLoaderService
+  ) {}
+
+  public ngOnInit(): void {
+    this.fetchSubjects();  
+  }
+
+  private fetchSubjects(): void {
+    this.preloaderService.show();
+    this.dataService.get(API_CONTACT_US_GET_SUBJECTS)
+      .then((response: IAPIResponse<IContactUsSubject[]>) => {
+        this.subjects = response.data;
+        console.log(this.subjects)
+        this.preloaderService.hide();
+      })
+      .catch((e) => {
+        console.error(e);
+        this.preloaderService.hide();
+      });
+  }
 }
